52a7a9e56beda23594d48bd6e6718523f186279e
Vorlent <vorlent@web.de>
2022-07-30 20:56:11 +0200

    Implement ThreadCoordinator and refactor static variables Part 2

--- src/main/java/org/jmt/mcmt/mixin/ThreadExecutorPatchMixin.java
+++ src/main/java/org/jmt/mcmt/mixin/ThreadExecutorPatchMixin.java
@@ -2,6 +2,7 @@ package org.jmt.mcmt.mixin;
 
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.thread.ThreadExecutor;
+import org.jmt.mcmt.ThreadCoordinator;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,24 +17,15 @@ import java.util.concurrent.ConcurrentHashMap;
 public abstract class ThreadExecutorPatchMixin {
 	@Shadow public abstract boolean isOnThread();
 
-	private static Map<String, Set<Thread>> mcThreadTracker = new ConcurrentHashMap<String, Set<Thread>>();
-
-	public static boolean isThreadPooled(String poolName, Thread t) {
-		return mcThreadTracker.containsKey(poolName) && mcThreadTracker.get(poolName).contains(t);
-	}
-
-	public static boolean serverExecutionThreadPatch(MinecraftServer ms) {
-		return isThreadPooled("MCMT", Thread.currentThread());
-	}
-
 	/**
 	 * Override isOnThread for MinecraftServer so that it returns true if the thread is in our threadpool
 	 */
 	@Inject(method = "isOnThread()Z", at = @At("HEAD"), cancellable = true)
 	private void isOnThread(CallbackInfoReturnable<Boolean> cir) {
+		final ThreadCoordinator threadCoordinator = ThreadCoordinator.getInstance();
 		if((Object)this instanceof MinecraftServer) {
 			MinecraftServer server = (MinecraftServer)(Object)this;
-			cir.setReturnValue(this.isOnThread() || serverExecutionThreadPatch(server));
+			cir.setReturnValue(this.isOnThread() || threadCoordinator.serverExecutionThreadPatch(server));
 		}
 	}
 }
