d42d01c8465721f2444b96d141418ee8ebd3608e
Vorlent <vorlent@web.de>
2023-02-07 18:36:17 +0100

    Add ServerWorldMixin for SynchronizedServerGetter

---
+++ src/main/java/org/boosted/mixin/getServer/ServerWorldMixin.java
@@ -0,0 +1,31 @@
+package org.boosted.mixin.getServer;
+
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.world.ServerWorld;
+import org.boosted.SynchronizedServerGetter;
+import org.boosted.unmodifiable.UnmodifiableMinecraftServer;
+import org.boosted.util.SynchronizedResource;
+import org.boosted.util.UnsynchronizedResource;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+
+@Mixin(ServerWorld.class)
+public class ServerWorldMixin implements SynchronizedServerGetter {
+	@Unique
+	private SynchronizedResource<MinecraftServer, UnmodifiableMinecraftServer> boosted$unsynchronizedResource;
+	public SynchronizedResource<MinecraftServer, UnmodifiableMinecraftServer> getSynchronizedServer() {
+		if (boosted$unsynchronizedResource == null) {
+			MinecraftServer server = ((SynchronizedServerGetter) (Object) this).getUnsynchronizedServer();
+			boosted$unsynchronizedResource = new UnsynchronizedResource<>(server,
+				new UnmodifiableMinecraftServer(server));
+		}
+		return boosted$unsynchronizedResource;
+	}
+
+	/**
+	 * Only evil people use this :(
+	 */
+	public MinecraftServer getUnsynchronizedServer() {
+		return ((ServerWorld) (Object) this).getServer();
+	}
+}
