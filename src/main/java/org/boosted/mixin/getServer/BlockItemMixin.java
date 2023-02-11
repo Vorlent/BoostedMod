96fe461d004fe03b416c4e1338d23d197cd743b2
Vorlent <vorlent@web.de>
2023-02-11 02:51:24 +0100

    Finish BlockItemMixin

--- src/main/java/org/boosted/mixin/getServer/BlockItemMixin.java
+++ src/main/java/org/boosted/mixin/getServer/BlockItemMixin.java
@@ -1,16 +1,28 @@
 package org.boosted.mixin.getServer;
 
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BlockItem.class)
 public class BlockItemMixin {
 
-    /*public static boolean writeNbtToBlockEntity(World world, @Nullable PlayerEntity player, BlockPos pos, ItemStack stack) {
-        MinecraftServer minecraftServer = world.getServer();
-        if (minecraftServer == null) {
-            return false;
+    @Inject(method = "writeNbtToBlockEntity(Lnet/minecraft/world/World;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/item/ItemStack;)Z",
+    cancellable = true, at = @At(value = "HEAD"))
+    private static void writeNbtToBlockEntity(World world, PlayerEntity player, BlockPos pos, ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
+        if (world instanceof ServerWorld serverWorld) {
+            if (serverWorld.getSynchronizedServer().readExp(server -> server == null)) {
+                cir.setReturnValue(false);
+            }
         }
-        ...
-    }*/
+    }
 }
