45cb906bd0529984d45225f00b971fd279dd9ede
Vorlent <vorlent@web.de>
2023-02-12 13:28:56 +0100

    Replace Overwrite with Inject

--- src/main/java/org/boosted/mixin/getServer/EntityMixin.java
+++ src/main/java/org/boosted/mixin/getServer/EntityMixin.java
@@ -2,7 +2,6 @@ package org.boosted.mixin.getServer;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.World;
@@ -11,50 +10,45 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Entity.class)
 public class EntityMixin {
 
-    /**
-     * @author Vorlent
-     * @reason tickPortal needs exclusive write access to MinecraftServer to obtain the destination world reference
+    /*
+        INVOKEVIRTUAL
+        INVOKEVIRTUAL
      */
-    @Overwrite
-    protected void tickPortal() {
+    @Redirect(method = "tickPortal()V",
+            at = @At(value = "INVOKE", target = "net/minecraft/server/world/ServerWorld.getServer ()Lnet/minecraft/server/MinecraftServer;"))
+    public MinecraftServer skipGetServer(ServerWorld instance) {
+        return null;
+    }
+
+    @Redirect(method = "tickPortal()V",
+            at = @At(value = "INVOKE", target = "net/minecraft/server/MinecraftServer.getWorld (Lnet/minecraft/util/registry/RegistryKey;)Lnet/minecraft/server/world/ServerWorld;"))
+    public @Nullable ServerWorld skipGetWorld(MinecraftServer instance, RegistryKey<World> key) {
+        return null;
+    }
+
+    @Inject(method = "tickPortal()V",
+        at = @At(value = "FIELD", target = "net/minecraft/entity/Entity.inNetherPortal : Z", ordinal = 1, shift = At.Shift.BEFORE))
+    public void injectTickPortal(CallbackInfo ci) {
         Entity entity = (Entity) (Object) this;
-        if (!(entity.world instanceof ServerWorld)) {
-            return;
-        }
-        int i = entity.getMaxNetherPortalTime();
         ServerWorld serverWorld = (ServerWorld)entity.world;
-        if (entity.inNetherPortal) {
-            /*
-            Patch BEGIN
-             */
-            serverWorld.getSynchronizedServer().write(minecraftServer -> {
-                ServerWorld serverWorld2 = minecraftServer.getWorld(entity.world.getRegistryKey() == World.NETHER ? World.OVERWORLD : World.NETHER);
-                if (serverWorld2 != null && minecraftServer.isNetherAllowed() && !entity.hasVehicle() && entity.netherPortalTime++ >= i) {
-                    entity.world.getProfiler().push("portal");
-                    entity.netherPortalTime = i;
-                    entity.resetPortalCooldown();
-                    entity.moveToWorld(serverWorld2);
-                    entity.world.getProfiler().pop();
-                }
-            });
-            /*
-            Patch END
-             */
-            entity.inNetherPortal = false;
-        } else {
-            if (entity.netherPortalTime > 0) {
-                entity.netherPortalTime -= 4;
+        serverWorld.getSynchronizedServer().write(minecraftServer -> {
+            int i = entity.getMaxNetherPortalTime();
+            ServerWorld serverWorld2 = minecraftServer.getWorld(entity.world.getRegistryKey() == World.NETHER ? World.OVERWORLD : World.NETHER);
+            if (serverWorld2 != null && minecraftServer.isNetherAllowed() && !entity.hasVehicle() && entity.netherPortalTime++ >= i) {
+                entity.world.getProfiler().push("portal");
+                entity.netherPortalTime = i;
+                entity.resetPortalCooldown();
+                entity.moveToWorld(serverWorld2);
+                entity.world.getProfiler().pop();
             }
-            if (entity.netherPortalTime < 0) {
-                entity.netherPortalTime = 0;
-            }
-        }
-        entity.tickPortalCooldown();
+        });
     }
 
     @Inject(method = "getServer()Lnet/minecraft/server/MinecraftServer;",
