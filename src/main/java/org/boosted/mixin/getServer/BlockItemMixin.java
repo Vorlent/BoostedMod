2bcbd0c83a51509180ffddc111fff1845c59cdc4
Vorlent <vorlent@web.de>
2023-02-13 19:49:17 +0100

    Update BlockItemMixin

--- src/main/java/org/boosted/mixin/getServer/BlockItemMixin.java
+++ src/main/java/org/boosted/mixin/getServer/BlockItemMixin.java
@@ -11,18 +11,30 @@ import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BlockItem.class)
 public class BlockItemMixin {
 
     @Inject(method = "writeNbtToBlockEntity(Lnet/minecraft/world/World;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/item/ItemStack;)Z",
-    cancellable = true, at = @At(value = "HEAD"))
+        cancellable = true, at = @At(value = "HEAD"))
     private static void writeNbtToBlockEntity(World world, PlayerEntity player, BlockPos pos, ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
         if (world instanceof ServerWorld serverWorld) {
             if (serverWorld.getSynchronizedServer().readExp(server -> server == null)) {
                 cir.setReturnValue(false);
             }
+        } else {
+            cir.setReturnValue(false);
         }
+    }
+
+    @Redirect(method = "writeNbtToBlockEntity(Lnet/minecraft/world/World;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/item/ItemStack;)Z",
+            at = @At(value = "INVOKE", target = "net/minecraft/world/World.getServer ()Lnet/minecraft/server/MinecraftServer;"))
+    private static MinecraftServer skipIf(World instance) {
+        // this reference does not leak
+        // it just bypasses the if statement, which has been reimplemented above
+        // also it is guaranteed to be of type ServerWorld as the above Inject returns if it is not
+        return ((ServerWorld)instance).getUnsynchronizedServer();
     }
 }
