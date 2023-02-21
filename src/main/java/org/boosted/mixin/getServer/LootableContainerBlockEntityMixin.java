c1c93f8b49c1bee77a28089fd4bc6fee30e3ed74
Vorlent <vorlent@web.de>
2023-02-21 19:25:09 +0100

    Update LootableContainerBlockEntity without @Overwrite

--- src/main/java/org/boosted/mixin/getServer/LootableContainerBlockEntityMixin.java
+++ src/main/java/org/boosted/mixin/getServer/LootableContainerBlockEntityMixin.java
@@ -1,51 +1,65 @@
 package org.boosted.mixin.getServer;
 
-import net.minecraft.advancement.criterion.Criteria;
 import net.minecraft.block.entity.LootableContainerBlockEntity;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.loot.LootManager;
 import net.minecraft.loot.LootTable;
 import net.minecraft.loot.context.LootContext;
-import net.minecraft.loot.context.LootContextParameters;
-import net.minecraft.loot.context.LootContextTypes;
-import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(LootableContainerBlockEntity.class)
 public class LootableContainerBlockEntityMixin {
 
-    @Shadow @Nullable public Identifier lootTableId;
+    private final ThreadLocal<Identifier> prevLootTableId = new ThreadLocal<>();
 
-    @Shadow public long lootTableSeed;
+    @Redirect(method = "checkLootInteraction(Lnet/minecraft/entity/player/PlayerEntity;)V",
+        at = @At(value = "INVOKE", target = "net/minecraft/world/World.getServer ()Lnet/minecraft/server/MinecraftServer;",
+        ordinal = 0))
+    public MinecraftServer redirectGetUnsynchronizedServer(World instance) {
+        if (instance instanceof ServerWorld serverWorld) {
+            return serverWorld.getUnsynchronizedServer();
+        }
+        return instance.getServer();
+    }
 
-    /**
-     * @author Vorlent
-     * @reason We need to wrap the entire method, because all of it depends on exclusive access on MinecraftServer
-     */
-    @Overwrite
-    public void checkLootInteraction(@Nullable PlayerEntity player) {
+    @Redirect(method = "checkLootInteraction(Lnet/minecraft/entity/player/PlayerEntity;)V",
+        at = @At(value = "INVOKE", target = "net/minecraft/world/World.getServer ()Lnet/minecraft/server/MinecraftServer;",
+        ordinal = 1))
+    public MinecraftServer skipGetServer(World instance) {
+        return null;
+    }
+
+    @Redirect(method = "checkLootInteraction(Lnet/minecraft/entity/player/PlayerEntity;)V",
+            at = @At(value = "INVOKE", target = "net/minecraft/server/MinecraftServer.getLootManager ()Lnet/minecraft/loot/LootManager;"))
+    public LootManager skipGetLootManager(MinecraftServer instance) {
+        return null;
+    }
+
+    @Redirect(method = "checkLootInteraction(Lnet/minecraft/entity/player/PlayerEntity;)V",
+            at = @At(value = "INVOKE", target = "net/minecraft/loot/LootManager.getTable (Lnet/minecraft/util/Identifier;)Lnet/minecraft/loot/LootTable;"))
+    public LootTable skipGetTable(LootManager instance, Identifier id) {
+        prevLootTableId.set(id);
+        return null;
+    }
+
+    @Redirect(method = "checkLootInteraction(Lnet/minecraft/entity/player/PlayerEntity;)V",
+        at = @At(value = "INVOKE", target = "net/minecraft/loot/LootTable.supplyInventory (Lnet/minecraft/inventory/Inventory;Lnet/minecraft/loot/context/LootContext;)V"))
+    public void redirectSupplyInventory(LootTable instance, Inventory inventory, LootContext context) {
+        Identifier identifier = prevLootTableId.get();
+        prevLootTableId.remove();
         LootableContainerBlockEntity entity = (LootableContainerBlockEntity)(Object)this;
+
         World world = entity.getWorld();
-        if (this.lootTableId != null && world instanceof ServerWorld serverWorld) {
+         if (world instanceof ServerWorld serverWorld) {
             serverWorld.getSynchronizedServer().write(server -> {
-                LootTable lootTable = server.getLootManager().getTable(this.lootTableId);
-                if (player instanceof ServerPlayerEntity) {
-                    Criteria.PLAYER_GENERATES_CONTAINER_LOOT.trigger((ServerPlayerEntity)player, this.lootTableId);
-                }
-                this.lootTableId = null;
-                LootContext.Builder builder = new LootContext.Builder((ServerWorld) world)
-                    .parameter(LootContextParameters.ORIGIN, Vec3d.ofCenter(entity.getPos()))
-                    .random(this.lootTableSeed);
-                if (player != null) {
-                    builder.luck(player.getLuck()).parameter(LootContextParameters.THIS_ENTITY, player);
-                }
-                lootTable.supplyInventory(entity, builder.build(LootContextTypes.CHEST));
+                LootTable lootTable = server.getLootManager().getTable(identifier);
+                lootTable.supplyInventory(inventory, context);
             });
         }
     }
